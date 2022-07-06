a632dad9cfb3829edbf9c6d9c6804d4298fb9c48
ritkd <ritesh.msit@gmail.com>
2022-07-07 01:00:55 +0530

    Added Floor of Number binary search function

---
+++ Q2FloorOfANumber.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace BinarySearchPractice_Video15
+{
+    internal class Q2FloorOfANumber
+    {
+        internal int GetFloorOfNumber(int[] arr1, int inp)
+        {
+            int start = 0;
+            int end = arr1.Length - 1;
+            int mid;
+            while (start <= end)
+            {
+                mid = start + (end - start) / 2;
+                if (arr1[mid] < inp)
+                {
+                    start = mid + 1;
+                }
+                else if (arr1[mid] > inp)
+                {
+                    end = mid - 1;
+                }
+                else
+                {
+                    return arr1[mid];
+                }
+            }
+            return arr1[end];
+        }
+    }
+}

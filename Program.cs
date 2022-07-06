825744c89d28bf3a22297c519b1cc77667b523c3
ritkd <ritesh.msit@gmail.com>
2022-07-06 19:09:19 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace BinarySearchPractice_Video15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr1 = new int[] { -6, -3, 0, 3, 5, 8, 12, 14, 15, 17, 24 };
+            Console.WriteLine("[{0}]", string.Join(", ", arr1));
+            Q1CeilingOfANumber q1 = new Q1CeilingOfANumber();
+            string cond = "";
+            while (cond!="n")
+            {
+                Console.Write("Enter Input for ceiling question:");
+                string input = Console.ReadLine();
+                int inp = Convert.ToInt32(input);
+                int result = q1.GetCeilingOfNumber(arr1, inp);
+                Console.WriteLine("Ceiling of the number {0} is {1}", input, result);
+                Console.Write("Wanna try again(y/n): ");
+                cond = Console.ReadLine();
+            }
+
+
+
+            Console.WriteLine("> Goodbye!");
+        }
+    }
+}
---
+++ Q1CeilingOfANumber.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace BinarySearchPractice_Video15
+{
+    /// <summary>
+    /// Ceiling of a number means 
+    /// to find the smallest number 
+    /// greater than or equal to the 
+    /// target
+    /// </summary>
+    internal class Q1CeilingOfANumber
+    {
+        internal int GetCeilingOfNumber(int[] arr1, int target)
+        {
+            int result = 0;           
+            int start = 0;
+            int end = arr1.Length-1;
+            while (start <= end)
+            {
+                int mid = start + (end - start) / 2; // in case the start + end exceeds range of int datatype
+                if (target < arr1[mid]) 
+                { 
+                    end = mid - 1;
+                }
+                else if (target>arr1[mid])
+                {
+                    start = mid + 1;
+                }
+                else
+                {
+                    return arr1[mid];
+                }
+            }
+            
+            return arr1[start];
+        }
+    }
+}
